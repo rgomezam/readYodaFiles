a65aa5d9ad9912c488139b6e091f31a51adb89e7
Raquel Gomez <raquel.gomez@cern.ch>
2015-09-08 14:21:16 +0200

    Create readYoda.cpp

---
+++ src/readYoda.cpp
@@ -0,0 +1,56 @@
+//============================================================================
+// Name        : readYODA3.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include<iostream>
+#include<fstream>
+using namespace std;
+//#include "Plot.h"
+#include "YodaFile.h"
+
+rivet::YodaFile p1("mcfilePow.yoda");
+rivet::YodaFile p2("mcfilePow-scale-1.yoda");
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	
+
+	
+
+
+	 p1.allocatePlots(); //YOU can only show headers after allocating them
+	 //	cout << "Number of plots: " <<  p1.getNumberOfPlots() << endl;
+	 p1.showAllPlots();
+
+	// cin.get(); //Pause
+	
+	 
+	 p2.allocatePlots(); //YOU can only show headers after allocating them
+	 //	cout << "Number of plots: " <<  p1.getNumberOfPlots() << endl;
+	 p2.showAllPlots();
+
+
+	 p1.comparePlotsTakeSmaller(p2);
+
+	 p2.comparePlotsTakeBigger(p1);
+
+	 cin.get(); //Pause
+
+	 rivet::YodaFile p3("./newFiles/biggest.yoda");
+	p3.showFile();
+
+	 cin.get(); //Pause
+	 rivet::YodaFile p4("./newFiles/smallest.yoda");
+
+	p4.showFile();
+
+	 return 0;
+}
